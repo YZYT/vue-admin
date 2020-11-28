a73ae130114f4d3bb71112b05e7b475883113142
YZYT <1565126883@qq.com>
2020-11-28 23:52:16 +0800

    配置路由 样式

---
+++ vue-admin/src/views/Login/login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="login">this is the login page<div>
+  <b-button>Button</b-button>
+  <b-button variant="danger">Button</b-button>
+  <b-button variant="success">Button</b-button>
+  <b-button variant="outline-primary">Button</b-button>
+</div></div>
+</template>
+<script>
+export default {
+    name: 'login'
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
