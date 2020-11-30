25802d1a667179e918f19c19a377310341eab6f5
YZYT <1565126883@qq.com>
2020-11-30 16:18:48 +0800

    backup

--- vue-admin/src/main.js
+++ vue-admin/src/main.js
@@ -7,10 +7,10 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// Vue.use(BootstrapVue)
-// Vue.use(IconsPlugin)
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(VueCompositionApi);
 Vue.config.productionTip = false;
 
